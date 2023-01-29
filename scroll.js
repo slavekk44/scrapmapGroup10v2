83d19db8922fe488ced9b4d06447f2ce983ce85b
Blast1113 <blast5761@gmail.com>
2023-01-29 18:51:38 +0000

    Compact scroller code + other changes

--- scroll.js
+++ scroll.js
@@ -4,54 +4,49 @@
 
 var scroller;
 
-var sections = [];
-var sectionPos = [];
-
 var pos = 0; // Current scroll position
 var secPos = 0; // Current section
 
-var windowHeight = 0;
+var windowHeight;
 
+/**
+ * Initialise/re-initialise the page scroller
+ * Either for page load or resolution changes
+ */
 function initScoller() {
-	scroller = document.getElementById('page-scroller');
-	if (!scroller) console.error("Scroller element not found (was the script loaded on the wrong page?)");
+	if (!scroller) {
+		// Get scoller element
+		scroller = document.getElementById('page-scroller');
+
+		// Error if not found
+		if (!scroller) console.error("Scroller element not found (was the script loaded on the wrong page?)");
+	}
+
+	// Update window height
 	windowHeight = window.visualViewport.height;
 
 	// Set body height to total number of sections
 	document.body.style.height = `${windowHeight*sections.length}px`;
 }
 
-// Set up arrays
+// Initialise on page load
 document.addEventListener('DOMContentLoaded', () => {
-	// Get page sections
-	sections = document.getElementsByClassName('page-section');
-
 	initScoller();
-
-	// Get section positions on page
-	for (var i=0; i<sections.length; i++) {
-		let s = sections[i];
-		sectionPos[i] = s.offsetTop;
-	}
 });
 
 // On scroll event; does most of the work
 document.addEventListener('scroll', () => {
+	// Update pos
 	pos = window.pageYOffset;
 
-	oldSecPos = secPos;
+	// Calculate the closest section
+	secPos = Math.round(pos / windowHeight);
 
-	secPos = Math.round(pos / windowHeight); // Calculate the closest section
-
-	let top = -(secPos*windowHeight);
-
-	console.log(top);
-
-	scroller.style.top = `${top}px`;
+	// Offset the page by the section position
+	scroller.style.top = `${-secPos*windowHeight}px`;
 });
 
 // Handle window resizes
 document.addEventListener('onresize', () => {
-	console.log("Resized to: ", window.visualViewport.height);
 	initScoller();
 });
